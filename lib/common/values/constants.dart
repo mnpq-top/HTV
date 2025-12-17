31208b5c56d1b7672929c0f72346909b2d8779b2
mnpq-top <118095436+mnpq-top@users.noreply.github.com>
2025-12-17 18:18:19 +0800

    Update constants.dart

--- lib/common/values/constants.dart
+++ lib/common/values/constants.dart
@@ -1,7 +1,7 @@
 /// 常量
 class Constants {
   /// 直播源
-  static const iptvSource = 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/zhumeng11/IPTV/main/IPTV.m3u';
+  static const iptvSource = 'http://iptv.m3u/m3u';
 
   /// 直播源缓存时间
   static const iptvSourceCacheKeepTime = 1000 * 60 * 60 * 24; // 24小时
@@ -13,7 +13,7 @@ class Constants {
   static const epgRefreshTimeThreshold = 6; // 不到6点不刷新
 
   /// github release latest
-  static const githubReleaseLatest = 'https://api.github.com/repos/yaoxieyoulei/my_tv/releases/latest';
+  static const githubReleaseLatest = 'https://api.github.com/repos/mnpq-top/HTV/releases/latest';
 
   /// github代理加速地址
   static const githubProxy = 'https://mirror.ghproxy.com/';
